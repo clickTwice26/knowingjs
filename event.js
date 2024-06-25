b30bf873f2bf59f5e14b18ea55d0d07049539b3e
clickTwice26 <shagatoc@gmail.com>
2024-06-25 12:31:01 +0600

    learned  "event"

---
+++ event.js
@@ -0,0 +1,6 @@
+"use strict"; // occurance
+console.log("Hello World"); 
+function showTime(){
+    window.alert("I am going to show the time.")
+    document.getElementById("event").innerHTML = Date();
+}

d2fc557affbe4598225ebc97b3d3c84a9e12933f
clickTwice26 <shagatoc@gmail.com>
2024-06-26 12:20:34 +0600

    string template completed

--- string.js
+++ string.js
@@ -1,3 +1,6 @@
-let text  = "Welcome to bangladesh";
-
-console.log(text.slice(-5, 0))
+let text = `Hello World!`; // double & single quoute niye jhamela na howar jonne
+let country = "Bangladesh";
+let sentence = `I love ` + country;
+let sentecewithInterpollation = `I love my country ${country}`;
+console.log(sentence);
+console.log(sentecewithInterpollation);
